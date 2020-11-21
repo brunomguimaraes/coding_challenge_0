2a929b86c47c8dc7424b4c308bccfabc6cf522fe
Bruno <mguimaraesbruno@gmail.com>
2020-11-21 08:46:12 -0300

    removes link temporary

--- src/views/NotFound/index.tsx
+++ src/views/NotFound/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { ReactComponent as NotFoundSvg } from 'assets/svg/not_found.svg';
 
 import { Header, Wrapper, SvgWrapper } from './style';
@@ -8,7 +7,6 @@ const NotFound = () => {
   return (
     <Wrapper>
       <Header>404 - Not Found!</Header>
-      <Link to="/">Go to Home</Link>
       <SvgWrapper>
         <NotFoundSvg />
       </SvgWrapper>
